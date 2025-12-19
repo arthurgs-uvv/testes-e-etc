583acaa9af88b8435a5a69dbc0646bdbb797773f
Arthur Gomes <arthuur.uvv@gmail.com>
2025-12-18 23:25:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function carregar(){    
+    var dataAtual = new Date()
+    var hora = dataAtual.getHours()
+    var textoHora = window.document.getElementById('textoHora')
+    var imgHora = window.document.getElementById('imgHora')
+
+    textoHora.innerHTML = `Agora são ${hora} horas`
+
+    if (hora <= 5){
+        imgHora.src = './imagens/img-madrugada.png'
+        document.body.style.background = '#000000ff'
+    }
+    else if (hora <= 11){
+        imgHora.src = './imagens/img-manha.jpg'
+        document.body.style.background = '#db8f00ff'
+    }
+    else if (hora <= 17){
+        imgHora.src = './imagens/img-tarde.jpg'
+        document.body.style.background = '#ffe600ff'
+    }
+    else{
+        imgHora.src = './imagens/img-noite.jpg'
+        document.body.style.background = '#020075ff'
+    }
+}
